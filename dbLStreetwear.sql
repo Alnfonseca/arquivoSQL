bfc4dd2a680c3c82c9a0c5af2157cbecbe03f76b
Alnfonseca <afonsecadesantana@gmail.com>
2023-10-20 17:10:05 -0300

    atualização botoes

--- dbLStreetwear.sql
+++ dbLStreetwear.sql
@@ -4,15 +4,25 @@ create database db_LStreetwear;
 
 use db_LStreetwear;
 
+create table tbLogin(
+usuario varchar(50) not null,
+senha varchar(10) not null
+
+);
+
 create table tbProdutos(
 codProd int not null auto_increment,
 nomeProd varchar(100) not null,
 marcaProd varchar(100) not null,
 quant int(50) not null,
-tamanho char(2) not null,
+tamanho varchar(2) not null,
 dataRep date not null,
 preco decimal(5,2) not null,
 primary key(codProd)
 );
 
-insert into tbProdutos (nomeProd, marcaProd, quant, tamanho, dataRep, preco) values ('Camisa Corinthans III', 'Nike', 20, 'G','2023/11/10' 299.99);
+insert into tbProdutos (nomeProd, marcaProd, quant, tamanho, dataRep, preco) values ('Camisa Corinthans III', 'Nike', 20, 'G','2023/11/10', 299.99);
+
+insert into	tbLogin (usuario, senha) values ('admin', 'admin');
+
+select codProd+1 from tbProdutos order by codProd desc;
